48d493cc72defd3f650aa6a1ff80d7907bd190a1
Joshua-Click <joshua.click25@gmail.com>
2023-07-18 16:50:31 -0500

    2nd part to exercise

---
+++ import_exercises.py
@@ -0,0 +1,74 @@
+# %%
+
+
+# %% [markdown]
+# Import and test 3 of the functions from your functions exercise file. Import each function in a different way:
+
+# %%
+
+
+# %% [markdown]
+#  a. Run an interactive python session and import the module. Call the is_vowel function using the . syntax.
+
+# %%
+import function_exercises as fe
+
+fe.is_vowel('a')
+
+# %% [markdown]
+# b. Create a file named import_exercises.py. Within this file, use from to import the calculate_tip function directly. Call this function with values you choose and print the result.
+
+from function_exercises import calculate_tip as calc_tip
+
+# %%
+
+# %% [markdown]
+# c .Create a jupyter notebook named import_exercises.ipynb. Use from to import the get_letter_grade function and give it an alias. Test this function in your notebook.
+
+# %%
+
+
+# %% [markdown]
+# Read about and use the itertools module from the python standard library to help you solve the following problems. Note: Many of these functions in this library return an object, to see the results of the function, cast this object as a list.
+#     - How many different ways can you combine a single letter from "abc" with either 1, 2, or 3?
+#     - How many different combinations are there of 2 letters from "abcd"?
+#     - How many different permutations are there of 2 letters from "abcd"?
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+# %% [markdown]
+# 
+
+
